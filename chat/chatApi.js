77ac79395775cd5682370233cfe81b76f5de6e76
PRATYUSH BANSAL <PRATYUSH BANSAL>
2016-08-15 19:25:44 +0530

    typing feature added, css changes

--- chat/chatApi.js
+++ chat/chatApi.js
@@ -4,11 +4,22 @@ var app=angular.module('chatApi',[]);
 app.controller('chatController',function($scope){
   $scope.message="";
   $scope.peopleOnlineCount="";
+  $scope.typing=false;
+  $scope.type="";
+  console.log(socket);
   $scope.submit=function(){
     socket.emit('message',$scope.message);
+    // socket.emit('yo',socket.)
+    $scope.message="";
+    $scope.type="";
   }
   socket.on('clientCount',function(count){
     $scope.peopleOnlineCount=count;
+  })
+  socket.on('check',function(client){
+    angular.forEach(client,function(value,key){
+      console.log(value);
+    })
   })
   socket.on('messageAll',function(msg){
     // angular.element('#chatBox').append(msg);
@@ -17,9 +28,44 @@ app.controller('chatController',function($scope){
     // box.css({
     //   "background-color":"yellow"
     // });
-    angular.element('#chatBox').append($('<ul>').text(msg));
+    // var m=$('<ul>');
+    // var i=$('<ul>');
+    // angular.element('#chatBox').append(i.text(socket.id));
+    // angular.element('#chatBox').append(m.text(msg));
+    // m.attr('id','text');
+    // i.attr('id','userId');
+    // angular.element('#text').addClass('yo');
+    // angular.element('#chatBox').append($('<ul>').text(socket.id));
+    angular.element('#chatBox').append($('<ul><br>').text(msg));
+
+    angular.element('ul').addClass('yo');
+
     // angular.element('#chatBox').addClass('yoman');
   })
-// $scope.
+  $scope.timeoutFunction = function(){
+    $scope.typing=false;
+    socket.emit('typing',false);
+  }
+  socket.on('typeAll',function(type){
+    console.log(type);
+    $scope.$apply(function(){
+      if(type)
+      $scope.type="typing";
+      else {
+      $scope.type="";
+      }
+    })
 
+  })
+  $('#message').keydown(function(){
+  if($scope.typing==false){
+    $scope.typing=true;
+    socket.emit('typing',true);
+    var time=setTimeout(function () {
+      $scope.timeoutFunction();
+    }, 3000);
+  }else{
+    clearTimeout(time);
+  }
+});
 })
--- server.js
+++ server.js
@@ -5,20 +5,34 @@ var app      = express();
 var server   = require('http').Server(app);
 var io       = require('socket.io')(server);
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+    // var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+mongoose.connect('mongodb://pratyush1995:chunmun1998@ds153735.mlab.com:53735/chit0chat');
+var Cat = mongoose.model('Cat',{name:String});
+var kitty = new Cat({name:'eshita'});
+kitty.save();
 app.get('/', function (req, res) {
   // res.send('Hello World!');
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  console.log(socket);
+  // console.log(socket.server.eio.clients);
   // var temp = JSON.parse(socket);
   // console.log(temp);
-  io.emit('clientCount',socket.server.clientsCount);
+  // console.log(socket.id);
+  // var tweet={'user':socket.server.eio.clients};
+  // io.emit('check',tweet);
+  io.emit('clientCount',socket.server.eio.clientsCount);
+  // io.emit('clientCount',socket.server.eio.clientsCount);
   socket.on('message',function(msg){
     console.log(msg);
     io.emit('messageAll',msg);
+  })
+  socket.on('typing',function(type){
+    io.emit('typeAll',type);
   })
   socket.on('disconnect', function() {
     console.log('disconnected');
